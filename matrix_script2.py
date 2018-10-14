0d6e9406dfe0049a805821c33bef724bf32e7c76
qaisqadri <qaisqadri7@gmail.com>
2018-10-14 11:28:21 +0530

    work on matrix_script, last given problem

---
+++ matrix_script2.py
@@ -0,0 +1,26 @@
+import re
+import string
+if __name__ == '__main__':
+	nm = input().split()
+
+	n = int(nm[0])
+
+	m = int(nm[1])
+
+	matrix = []
+
+	for _ in range(n):
+	    matrix_item = input()
+	    matrix.append(matrix_item)
+	# print(matrix)
+	st=''
+	for y in range(m):
+		for x in range(n):
+			st=st+matrix[x][y]
+
+print(st)
+# st=re.sub(r'['+string.punctuation+']',' ',st)
+st=re.sub(r'[!@#$%&]'," ",st)
+st = re.sub(r'\s+', ' ', st)           # Eliminate duplicate whitespaces
+
+print(st)
