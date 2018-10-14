3b0b4ea2d7d78e48b587eb5f86e65d765b4cd9c4
qaisqadri <qaisqadri7@gmail.com>
2018-10-14 20:36:40 +0530

    finished

--- matrix_script.py
+++ matrix_script.py
@@ -1,26 +1,20 @@
 import re
-import string
-if __name__ == '__main__':
-	nm = input().split()
+nm = input().split()
 
-	n = int(nm[0])
+n = int(nm[0])
 
-	m = int(nm[1])
+m = int(nm[1])
 
-	matrix = []
+matrix = []
 
-	for _ in range(n):
-	    matrix_item = input()
-	    matrix.append(matrix_item)
-	# print(matrix)
-	st=''
-	for y in range(m):
-		for x in range(n):
-			st=st+matrix[x][y]
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+#print(matrix)
+st=''
+for y in range(m):
+    for x in range(n):
+        st=st+matrix[x][y]
 
-# print(st)
-st=re.sub(r'(?<=\w)([\W]+)(?=\w)',' ',st)
-# st=re.sub(r'[!@#$%&]'," ",st)
-# st = re.sub(r'\s+', ' ', st)           # Eliminate duplicate whitespaces
-
+st=re.sub("(?<=\w)([\W]+)(?=\w)"," ",st)
 print(st)
